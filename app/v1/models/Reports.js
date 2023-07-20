e226abcac4e6d89071c1924e0af5e96a5988eb00
Ajmal Nasumudeen <ajmal.nasumudeen@treeswise.com>
2023-07-20 18:28:14 +0530

    added report model

--- app/v1/models/Reports.js
+++ app/v1/models/Reports.js
@@ -1,23 +1,23 @@
 import mongoose from "mongoose";
 
 const Schema = new mongoose.Schema({
-    user: {
+    vesselId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+        ref: "User",
+        index: true
     },
-    inspectionDetails: {
-        inspection_date: { type: Number, default: "" },
-        normal_service_load_in_percent_MCRMCR: { type: String, default: "" },
-        total_running_hours: { type: String, default: "" },
-        running_hrs_since_last: { type: String, default: "" },
-        cyl_oil_Type: { type: String, default: "" },
-        cyl_oil_consump_Ltr_24hr: { type: String, default: "" },
-        normal_service_load_in_percent_MCR: { type: String, default: "" },
-        cylinder_numbers: { type: String, default: "" }
-    }
+    inspection_date: { type: Number, default: 0, index: true },
+    normal_service_load_in_percent_MCRMCR: { type: String, default: "" },
+    total_running_hours: { type: String, default: "" },
+    running_hrs_since_last: { type: String, default: "" },
+    cyl_oil_Type: { type: String, default: "" },
+    cyl_oil_consump_Ltr_24hr: { type: String, default: "" },
+    normal_service_load_in_percent_MCR: { type: String, default: "" },
+    cylinder_numbers: { type: Number }
+    // ...other data
 
 });
-const User = mongoose.model("Reports", Schema);
+const Report = mongoose.model("Reports", Schema);
 
-export default User;
+export default Report;
 
