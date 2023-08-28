0f1ad2c9b18c1b139f83c9185014c6695cd68761
Ajmal Nasumudeen <ajmal.nasumudeen@treeswise.com>
2023-08-28 17:17:48 +0530

    configuration for deployment

--- app/v1/routes/admin/adminRoutes.js
+++ app/v1/routes/admin/adminRoutes.js
@@ -1,10 +1,8 @@
 import express from "express";
-
 import { createOrg, dashboardList, usersList } from "../../controllers/admin/adminController.js";
 
 
 const router = express.Router();
-
 
 router.post("/create-org", createOrg);
 router.get("/users-all", usersList);
