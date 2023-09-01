ad1bce333c737823cb48aaaea68b0d366daa6de7
Shilpa_Reji <shilpa.reji@treeswise.com>
2023-09-01 14:25:08 +0530

    dashboard report and image count api

--- app/v1/controllers/admin/adminController.js
+++ app/v1/controllers/admin/adminController.js
@@ -13,16 +13,25 @@ export const dashboardList = async (req, res) => {
         const organizations = await Organization.countDocuments();
         const totalUsers = await User.countDocuments({ userType: { $ne: "Admin" } });
         const vessels = await User.countDocuments({ userType: "Vessel" });
-        const reports = await Report.countDocuments({});
         const fleetManagers = await User.countDocuments(condClause);
-        const cylinderImageCount = 0; // TODO
         const data = {
             organizations,
             totalUsers,
             vessels,
-            reports,
-            cylinderImageCount,
             fleetManagers
+        };
+        res.status(200).json({ data });
+    } catch (error) {
+        res.status(500).json({ errorTitle: ERROR_MSG.SOMETHING_WENT, message: error.message });
+    }
+};
+export const dashboardReportImageCount = async (req, res) => {
+    try {
+        const reports = await Report.countDocuments();
+        const cylinderImageCount = 0; // TODO
+        const data = {
+            reports,
+            cylinderImageCount
         };
         res.status(200).json({ data });
     } catch (error) {
