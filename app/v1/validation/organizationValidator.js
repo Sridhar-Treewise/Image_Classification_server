7073f3d775372ac4503ca87b063580ef04eb3cc8
Shilpa_Reji <shilpa.reji@treeswise.com>
2023-09-08 12:04:34 +0530

    vessel list validation

--- app/v1/validation/organizationValidator.js
+++ app/v1/validation/organizationValidator.js
@@ -1,7 +1,11 @@
 import { validator } from "../../utils/validators.js";
-import { createVesselSchema } from "./schema/organizationValidatorSchema.js";
+import { createVesselSchema, vesselSchema } from "./schema/organizationValidatorSchema.js";
+import { queryValidator } from "../validation/validator.js";
 
 
 export const createVesselValidate = (req, res, next) => {
     validator(createVesselSchema, req.body, next);
+};
+export const vesselListValidation = (req, res, next) => {
+    queryValidator(vesselSchema, req.query, next);
 };
