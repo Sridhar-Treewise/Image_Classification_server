aa5b1c62a0ec91cecf8d940875d2f80b5e95a9d5
Shilpa Reji <shilpa.reji@treeswise.com>
2023-11-17 18:56:03 +0530

    dashboard api changes

--- app/v1/controllers/admin/adminController.js
+++ app/v1/controllers/admin/adminController.js
@@ -6,21 +6,40 @@ import Organization from "../../models/Organizations.js";
 import Subscription from "../../models/Subscriptions.js";
 import { SUBSCRIPTION_MODEL } from "../../../common/constants.js";
 
-export const dashboardList = async (req, res) => {
+export const dashboardSubscription = async (req, res) => {
+    try {
+        const organizations = await Organization.countDocuments();
+        const freeTrail = await Subscription.distinct("orgCode", { plan: SUBSCRIPTION_MODEL.FREE });
+        const basic = await Subscription.distinct("orgCode", { plan: SUBSCRIPTION_MODEL.BASIC });
+        const pro = await Subscription.distinct("orgCode", { plan: SUBSCRIPTION_MODEL.PRO });
+        const premium = await Subscription.distinct("orgCode", { plan: SUBSCRIPTION_MODEL.PREMIUM });
+        const series = [freeTrail.length, basic.length, pro.length, premium.length];
+        const labels = ["freeTrail", "basic", "pro", "premium"];
+        const data = {
+            organizations,
+            series,
+            labels
+        };
+        res.status(200).json({ data });
+    } catch (error) {
+        res.status(500).json({ errorTitle: ERROR_MSG.SOMETHING_WENT, message: error.message });
+    }
+};
+export const dashboardUsersList = async (req, res) => {
     try {
         const condClause = {
             userType: "Organization",
             designation: "FLEET_MANAGER"
         };
-        const organizations = await Organization.countDocuments();
         const totalUsers = await User.countDocuments({ userType: { $ne: "Admin" } });
         const vessels = await User.countDocuments({ userType: "Vessel" });
         const fleetManagers = await User.countDocuments(condClause);
+        const series = [vessels, fleetManagers];
+        const labels = ["Fleet Managers", "Vessels"];
         const data = {
-            organizations,
             totalUsers,
-            vessels,
-            fleetManagers
+            series,
+            labels
         };
         res.status(200).json({ data });
     } catch (error) {
